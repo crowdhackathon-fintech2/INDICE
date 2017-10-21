ba66fa83aa428499a509bf39fbb2797f08df048b
Αλέξης Φίλος <a.filos@indice.gr>
2017-10-21 16:19:13 +0300

    Created NbgBankProvider.

---
+++ src/Incontrl.Csl/Provider/Concrete/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Incontrl.Provider.Concrete
+{
+    public class Transaction
+    {
+        public TransactionDetails Details { get; set; }   
+
+        public string Id { get; set; }
+    }
+    
+    public class TransactionDetails
+    {
+        public TransactionValue Value { get; set; }
+
+        public DateTime Completed { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public class TransactionValue
+    {
+        public string Currency { get; set; }
+
+        public decimal Ammount { get; set; }
+    }
+}
--- src/Incontrl.Csl/Provider/IBankProvider.cs
+++ src/Incontrl.Csl/Provider/IBankProvider.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Incontrl.Net.Models;
 
 namespace Incontrl.Provider
 {
     public interface IBankProvider
     {
-        IEnumerable<BankTransaction> GetTransactions(BankTransactionSearchDocument searchDoc);
+        Task<IEnumerable<BankTransaction>> GetTransactionsAsync(BankTransactionSearchDocument searchDoc);
     }
 }
